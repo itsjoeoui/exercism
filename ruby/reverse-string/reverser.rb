cdfe5d8527b1932702425c0c97dcbd85fc137270
Joey Yu <joey@jyu.dev>
2024-07-06 14:24:43 -0400

    feat(ruby): 2 exercises

---
+++ ruby/reverse-string/reverser.rb
@@ -0,0 +1,11 @@
+# Write your code for the 'Reverse String' exercise in this file. Make the tests in
+# `reverse_string_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/reverse-string` directory.
+
+module Reverser
+  def self.reverse(input)
+    input.reverse
+  end
+end
