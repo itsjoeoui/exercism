64a716caa0f48a06e12a3c4928836afd4edc668a
Joey Yu <joey@jyu.dev>
2024-07-23 16:29:48 -0400

    feat(ruby): robot-name

---
+++ ruby/robot-name/robot_name.rb
@@ -0,0 +1,21 @@
+require 'set'
+
+class Robot
+  attr_reader :name
+
+  def initialize
+    reset
+  end
+
+  def reset
+    @name = Robot.random_names.next
+  end
+
+  def self.forget
+    random_names.rewind
+  end
+
+  def self.random_names
+    @@all_names ||= ('AA000'..'ZZ999').to_a.shuffle.to_enum
+  end
+end
