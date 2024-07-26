3f3bb482aa40f90ba176dbd03cddc941e79a4a6d
Joey Yu <joey@jyu.dev>
2024-07-26 11:21:18 -0400

    feat(go): 2 exercises

---
+++ go/blackjack/blackjack.go
@@ -0,0 +1,56 @@
+package blackjack
+
+// ParseCard returns the integer value of a card following blackjack ruleset.
+func ParseCard(card string) int {
+	switch card {
+	case "ace":
+		return 11
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	case "ten":
+		return 10
+	case "jack":
+		return 10
+	case "queen":
+		return 10
+	case "king":
+		return 10
+	default:
+		return 0
+	}
+}
+
+// FirstTurn returns the decision for the first turn, given two cards of the
+// player and one card of the dealer.
+func FirstTurn(card1, card2, dealerCard string) string {
+	handScore := ParseCard(card1) + ParseCard(card2)
+	dealerScore := ParseCard(dealerCard)
+
+	switch {
+	case handScore == 22:
+		return "P"
+	case handScore == 21:
+		if dealerScore < 10 {
+			return "W"
+		}
+		return "S"
+	case handScore >= 17 || (handScore >= 12 && dealerScore < 7):
+		return "S"
+	default:
+		return "H"
+	}
+}
