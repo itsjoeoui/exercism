3d6ad2312370fa95862e420f5420b8148e477332
Joey Yu <joey@jyu.dev>
2024-07-31 10:56:57 -0400

    feat(go): bird-watcher

---
+++ go/bird-watcher/bird_watcher.go
@@ -0,0 +1,34 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	total := 0
+	for _, count := range birdsPerDay {
+		total += count
+	}
+	return total
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	total := 0
+
+	startOfWeek := (week - 1) * 7
+	endOfWeek := startOfWeek + 7
+	for i := startOfWeek; i < endOfWeek; i++ {
+		total += birdsPerDay[i]
+	}
+
+	return total
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := 0; i < len(birdsPerDay); i += 2 {
+		birdsPerDay[i]++
+	}
+	return birdsPerDay
+}
