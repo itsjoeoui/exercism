56645188ad993aa1b370c09652c05ee7b81b9d32
Joey Yu <joey@jyu.dev>
2024-08-25 20:47:40 -0400

    feat(rust): 3 exercises

---
+++ rust/beer-song/src/lib.rs
@@ -0,0 +1,27 @@
+pub fn verse(n: u32) -> String {
+    match n {
+        0 => String::from(
+            "No more bottles of beer on the wall, no more bottles of beer.\n\
+            Go to the store and buy some more, 99 bottles of beer on the wall.\n",
+        ),
+        1 => String::from(
+            "1 bottle of beer on the wall, 1 bottle of beer.\n\
+            Take it down and pass it around, no more bottles of beer on the wall.\n",
+        ),
+        _ => format!(
+            "{0} bottles of beer on the wall, {0} bottles of beer.\n\
+            Take one down and pass it around, {1} bottle{2} of beer on the wall.\n",
+            n,
+            n - 1,
+            if n == 2 { "" } else { "s" }
+        ),
+    }
+}
+
+pub fn sing(start: u32, end: u32) -> String {
+    (end..=start)
+        .rev()
+        .map(verse)
+        .collect::<Vec<String>>()
+        .join("\n")
+}
---
+++ rust/grains/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn square(s: u32) -> u64 {
+    assert!((1..=64).contains(&s), "Square must be between 1 and 64");
+
+    1 << (s - 1)
+}
+
+pub fn total() -> u64 {
+    (1..=64).map(square).sum()
+}
